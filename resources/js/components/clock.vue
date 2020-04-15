5314d7b58eb24af656819841a587a25f1043f208
Charlie Ontiveros <dev@charlietheprogrammer.com>
2020-04-14 18:13:07 -0700

    Added clock widget. CSS updates.

---
+++ resources/js/components/clock.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="clock" class="display-1">
+      {{ date | toTimeFormat }}
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            date: {}
+        }
+    },
+    created() {
+        this.date = new Date();
+        setInterval(()=>{
+            this.date = new Date();
+        }, 300);
+    },
+    filters: {
+        toTimeFormat(date) {
+            const time = date.toLocaleTimeString();
+            return  time.substring(0, time.lastIndexOf(':')) +  time.substring(time.lastIndexOf(':') + 3);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,10 +24,10 @@
 		<div class="flex-center position-ref full-height">
 			<div class="content">
 				<div id="welcome-box">
-					<div class="title m-b-md text-white font-weight-bold">
+					<div class="title m-b-md font-weight-bold">
 						Welcome User
 					</div>
-					<div class="h2 mb-3 text-white font-weight-bold">
+					<div class="h2 mb-3 font-weight-bold">
 						What would you like to see today?
 					</div>
 					<form method="POST" action="/">
@@ -38,6 +38,7 @@
 						<div><button class="btn btn-primary">Submit</button></div>
 					</form>
 				</div>
+				<clock></clock>
 
 				<div id="footer" class="fixed-bottom pb-2 text-light">
 					<table class="w-100">
