db1309fe07f571582b7146113308154cf2b93f46
Charlie Ontiveros <dev@charlietheprogrammer.com>
2020-06-18 20:55:58 -0700

    Updating config to resolve weird 'this' issues as a result of hot loading.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  devServer: {
+    hot: false,
+    hotOnly: false,
+    liveReload: false
+  },
+  // Turning off hot reload because it fucks with document related functions
+  chainWebpack: config => {
+    config.plugins.delete('hmr');
+  }
 };
